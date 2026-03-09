606b37e88836301b1ea54fcf0b75c78f9799ce91
mattyatea <mattyacocacora0@gmail.com>
2026-03-10 07:17:58 +0900

    Fix: 最新版になってmediaTLがぶっ壊れたので修正

--- packages/backend/src/server/api/endpoint-list.ts
+++ packages/backend/src/server/api/endpoint-list.ts
@@ -333,6 +333,7 @@ export * as 'notes/featured' from './endpoints/notes/featured.js';
 export * as 'notes/global-timeline' from './endpoints/notes/global-timeline.js';
 export * as 'notes/hybrid-timeline' from './endpoints/notes/hybrid-timeline.js';
 export * as 'notes/local-timeline' from './endpoints/notes/local-timeline.js';
+export * as 'notes/media-timeline' from './endpoints/notes/media-timeline.js';
 export * as 'notes/mentions' from './endpoints/notes/mentions.js';
 export * as 'notes/polls/recommendation' from './endpoints/notes/polls/recommendation.js';
 export * as 'notes/polls/vote' from './endpoints/notes/polls/vote.js';
